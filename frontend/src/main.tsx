efbe7db3dd7cd0b12a89f91930222f9fc81bf75f
SaintlyCoder <124301059+SaintlyCoder@users.noreply.github.com>
2025-06-24 19:43:37 +0100

    chore: use NEXT_PUBLIC_API_BASE_URL

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -12,7 +12,7 @@ import { routeTree } from "./routeTree.gen"
 import { ApiError, OpenAPI } from "./client"
 import { CustomProvider } from "./components/ui/provider"
 
-OpenAPI.BASE = import.meta.env.VITE_API_URL
+OpenAPI.BASE = process.env.NEXT_PUBLIC_API_BASE_URL as string
 OpenAPI.TOKEN = async () => {
   return localStorage.getItem("access_token") || ""
 }
